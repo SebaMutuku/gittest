2a3d01f287a8d43fffcf24125301c1577a03b15b
SebaMutuku <sebastian.muthiani@gmail.com>
2018-09-21 19:58:50 +0300

    first commit

---
+++ WebApp/Web/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import url
+from . import views
+from django.contrib.auth import login
+
+urlpatterns=[
+    url(r'^$',views.home),
+    url(r'^login/$',login,{'template_name':'Web/login.html'})
+]
